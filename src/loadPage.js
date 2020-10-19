339d88f4c4bcb138d2114f7822c4e81c6d64581b
Anna <annagiulia.musso@gmail.com>
2020-10-19 09:59:39 -0500

    added tab buttons

--- src/loadPage.js
+++ src/loadPage.js
@@ -5,6 +5,27 @@ const loadPage = () => {
     const h1 = document.createElement("h1");
     h1.textContent = "The Pasta Society";
     content.appendChild(h1);
+
+    const tab = document.createElement("div");
+    tab.classList.add("tab");
+    content.appendChild(tab);
+
+    const createTabBtns = (() => {
+        for (let i = 0; i <3; i++) {
+            const button = document.createElement("button");  
+            button.classList.add("tablinks");          
+          // button.textContent = "Home";
+            tab.appendChild(button);
+        }
+    })();
+    
+    const addBtnsContent = (() => {
+        const buttons = document.querySelectorAll("button");
+        buttons.item(0).textContent = "Home";
+        buttons.item(1).textContent = "Menu";
+        buttons.item(2).textContent = "Contact";
+    })();
+    
     
     const img = document.createElement("img");
     img.src = "./pasta-pic.jpg";    
